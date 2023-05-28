166a7b56a7ae864ed690e57d9825ab4dadd91cc9
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2023-05-28 10:41:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Head from './component/Head'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Head></Head>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Head.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Head.css'
+
+const Head = () => {
+  return (
+    <div>
+      <nav>
+        <a href="Services">Services</a>
+        <a href="Portfolio">Portfolio</a>
+        <a href="Package">Package</a>
+        <a href="Clients">Clients</a>
+        <a href="Career">Career</a>
+        <a href="AboutUs">About Us</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Head;
